b796bc6e2778ae9a8a6c32b746d92744476e1408
loganatkinson <logan.atkinson@gmail.com>
2021-06-12 19:55:01 -0400

    agh calling it for the night

--- mystery.py
+++ mystery.py
@@ -5,19 +5,38 @@ file = open('words.txt', 'r')
 list = file.read().lower().split()
 #print(list)
 
-guess_list = []
-
 selected_word = random.choice(list)
+selected_word_list = selected_word.split()
 #print('selected word is: ', selected_word)
 
+def play(selected_word):
+    incorrect_guesses = []
+    guess_input()
+    display_word(selected_word_list, guess_list)
+    if guesses == 8:
+        print(f"Game Over. The mystery word was {selected_word}")
+    elif guesses < 0 and "_" in selected_word_list:
+        print(f"Mystery Word: {' '.join(display_word(selected_word_list, guess_list))}")
+        print(f"You've already guessed: {' '.join(incorrect_guesses)}")
+        print(f"You have {guesses_available} guesses left")
+    elif guesses < 8 and "_" not in selected_word_list:
+        print(f"Yay! you guessed the Mystery Word {selected_word}")
+
 def guess_input():
+    guess_list = []
+    guesses = 0
     guess = input("Choose a letter").lower()
     if len(guess) != 1:
         print("You may only guess 1 letter at a time")
     else:
-        guess_list.apphend(guess)
-    return guess_list
+        guesses += 1
+        guess_list.append(guess)
+    
 
-def display_word(selected_word, guess_list):
-    return[letter if letter in guess_list else '_' for letter in selected_word]
+def display_word(selected_word_list, guess_list):
+    print([letter if letter in guess_list else '_' for letter in selected_word_list])
 
+
+
+
+play(selected_word)
