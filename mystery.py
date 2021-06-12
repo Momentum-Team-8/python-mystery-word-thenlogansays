88c7cbd3e20f364facb5646ddf806979684d29d8
loganatkinson <logan.atkinson@gmail.com>
2021-06-12 16:56:55 -0400

    functions for getting guess input and for displaying mystery word

--- mystery.py
+++ mystery.py
@@ -1,9 +1,23 @@
 import random
 
-file = open('words.txt', 'r')
 
+file = open('words.txt', 'r')
 list = file.read().lower().split()
 #print(list)
 
+guess_list = []
+
 selected_word = random.choice(list)
-print(selected_word)
+#print('selected word is: ', selected_word)
+
+def guess_input():
+    guess = input("Choose a letter").lower()
+    if len(guess) != 1:
+        print("You may only guess 1 letter at a time")
+    else:
+        guess_list.apphend(guess)
+    return guess_list
+
+def display_word(selected_word, guess_list):
+    return[letter if letter in guess_list else '_' for letter in selected_word]
+
